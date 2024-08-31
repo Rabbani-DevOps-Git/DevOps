6c007bb56decb04bb05cac07e2e71fc17a4be722
Syed Rabbani <syedrabbani.mw@gmail,com>
2024-08-31 18:51:45 +0400

    conversation

--- 02-converstation.sh
+++ 02-converstation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "$1:: Hi Suresh, How are you?"
-echo "$2:: Hello Ramesh. I am fine. How are you doing?"
+echo "$1:: Hi $1, How are you?"
+echo "$2:: Hello $2. I am fine. How are you doing?"
 echo "$3:: I am doing good Suresh. What's going on?"
 echo "$4:: I started learning Shell Script Ramesh"
